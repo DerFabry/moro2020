e83718e53da4b2871a3037a85ce6cf28b56892f2
Stephan Fabry <stephan.fabry@stud.hs-emden-leer.de>
2020-09-24 15:07:35 +0200

    generated Images again

---
+++ convertMeasurements.m
@@ -0,0 +1,66 @@
+distanceArray = [25, 50, 75, 100, 150, 200, 300, 400, 500, 600, 700, 850, 1000, 1150, 1300, 1500];
+InfraredSensorArray(:,1:9,:)
+
+AllIRMesaurmentsforDistance25 = reshape(InfraredSensorArray(1,1:9,:), [1,900]);
+AllLAMesaurmentsforDistance25 = reshape(LaserSensorArray(1,1:9,:), [1,900]);
+AllUSMesaurmentsforDistance25 = reshape(UltrasonicSensorArray(1,1:9,:), [1,900]);
+
+AllIRMesaurmentsforDistance50 = reshape(InfraredSensorArray(2,1:9,:), [1,900]);
+AllLAMesaurmentsforDistance50 = reshape(LaserSensorArray(2,1:9,:), [1,900]);
+AllUSMesaurmentsforDistance50 = reshape(UltrasonicSensorArray(2,1:9,:), [1,900]);
+
+AllIRMesaurmentsforDistance75 = reshape(InfraredSensorArray(3,1:9,:), [1,900]);
+AllLAMesaurmentsforDistance75 = reshape(LaserSensorArray(3,1:9,:), [1,900]);
+AllUSMesaurmentsforDistance75 = reshape(UltrasonicSensorArray(3,1:9,:), [1,900]);
+
+AllIRMesaurmentsforDistance100 = reshape(InfraredSensorArray(4,1:9,:), [1,900]);
+AllLAMesaurmentsforDistance100 = reshape(LaserSensorArray(4,1:9,:), [1,900]);
+AllUSMesaurmentsforDistance100 = reshape(UltrasonicSensorArray(4,1:9,:), [1,900]);
+
+AllIRMesaurmentsforDistance150 = reshape(InfraredSensorArray(5,1:9,:), [1,900]);
+AllLAMesaurmentsforDistance150 = reshape(LaserSensorArray(5,1:9,:), [1,900]);
+AllUSMesaurmentsforDistance150 = reshape(UltrasonicSensorArray(5,1:9,:), [1,900]);
+
+AllIRMesaurmentsforDistance200 = reshape(InfraredSensorArray(6,1:9,:), [1,900]);
+AllLAMesaurmentsforDistance200 = reshape(LaserSensorArray(6,1:9,:), [1,900]);
+AllUSMesaurmentsforDistance200 = reshape(UltrasonicSensorArray(6,1:9,:), [1,900]);
+
+AllIRMesaurmentsforDistance300 = reshape(InfraredSensorArray(7,1:9,:), [1,900]);
+AllLAMesaurmentsforDistance300 = reshape(LaserSensorArray(7,1:9,:), [1,900]);
+AllUSMesaurmentsforDistance300 = reshape(UltrasonicSensorArray(7,1:9,:), [1,900]);
+
+AllIRMesaurmentsforDistance400 = reshape(InfraredSensorArray(8,1:9,:), [1,900]);
+AllLAMesaurmentsforDistance400 = reshape(LaserSensorArray(8,1:9,:), [1,900]);
+AllUSMesaurmentsforDistance400 = reshape(UltrasonicSensorArray(8,1:9,:), [1,900]);
+
+AllIRMesaurmentsforDistance500 = reshape(InfraredSensorArray(9,1:9,:), [1,900]);
+AllLAMesaurmentsforDistance500 = reshape(LaserSensorArray(9,1:9,:), [1,900]);
+AllUSMesaurmentsforDistance500 = reshape(UltrasonicSensorArray(9,1:9,:), [1,900]);
+
+AllIRMesaurmentsforDistance600 = reshape(InfraredSensorArray(10,1:9,:), [1,900]);
+AllLAMesaurmentsforDistance600 = reshape(LaserSensorArray(10,1:9,:), [1,900]);
+AllUSMesaurmentsforDistance600 = reshape(UltrasonicSensorArray(10,1:9,:), [1,900]);
+
+AllIRMesaurmentsforDistance700 = reshape(InfraredSensorArray(11,1:9,:), [1,900]);
+AllLAMesaurmentsforDistance700 = reshape(LaserSensorArray(11,1:9,:), [1,900]);
+AllUSMesaurmentsforDistance700 = reshape(UltrasonicSensorArray(11,1:9,:), [1,900]);
+
+AllIRMesaurmentsforDistance850 = reshape(InfraredSensorArray(12,1:9,:), [1,900]);
+AllLAMesaurmentsforDistance850 = reshape(LaserSensorArray(12,1:9,:), [1,900]);
+AllUSMesaurmentsforDistance850 = reshape(UltrasonicSensorArray(12,1:9,:), [1,900]);
+
+AllIRMesaurmentsforDistance1000 = reshape(InfraredSensorArray(13,1:9,:), [1,900]);
+AllLAMesaurmentsforDistance1000 = reshape(LaserSensorArray(13,1:9,:), [1,900]);
+AllUSMesaurmentsforDistance1000 = reshape(UltrasonicSensorArray(13,1:9,:), [1,900]);
+
+AllIRMesaurmentsforDistance1150 = reshape(InfraredSensorArray(14,1:9,:), [1,900]);
+AllLAMesaurmentsforDistance1150 = reshape(LaserSensorArray(14,1:9,:), [1,900]);
+AllUSMesaurmentsforDistance1150 = reshape(UltrasonicSensorArray(14,1:9,:), [1,900]);
+
+AllIRMesaurmentsforDistance1300 = reshape(InfraredSensorArray(15,1:9,:), [1,900]);
+AllLAMesaurmentsforDistance1300 = reshape(LaserSensorArray(15,1:9,:), [1,900]);
+AllUSMesaurmentsforDistance1300 = reshape(UltrasonicSensorArray(15,1:9,:), [1,900]);
+
+AllIRMesaurmentsforDistance1500 = reshape(InfraredSensorArray(16,1:9,:), [1,900]);
+AllLAMesaurmentsforDistance1500 = reshape(LaserSensorArray(16,1:9,:), [1,900]);
+AllUSMesaurmentsforDistance1500 = reshape(UltrasonicSensorArray(16,1:9,:), [1,900]);
